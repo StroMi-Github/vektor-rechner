aef75fe8210f05f1926d5b9a892a320706f2963c
Domecheck14 <61684442+Domecheck14@users.noreply.github.com>
2020-03-02 10:06:33 +0100

    Update vektor.js

--- vektor.js
+++ vektor.js
@@ -1,1 +1,25 @@
 // Hier wird der Vektor entstehen.
+// Die Funktionen sind:
+/*Eingabe von Vektoren  
+Klasse Vektor 
+Beliebig viele Vektoren (Jeden Vektor einzeln + Button “Hinzufügen”) 
+Geraden , Ebenen , Zahlen 
+Grafische Oberfläche zur Eingabe 
+In einer Grafik Anzeigen 
+X-Y-Z Ansicht 
+(Andere Ansichten) 
+Liste am Rand der Ansicht (Tabellenartig) + “Löschen” Button 
+Berechnung von 2D und 3D Vektoren 
+Addition 
+Subtraktion 
+Skalarprodukt 
+Kreuzprodukt 
+Einheitsvektor 
+Mittelvektor 
+Länge 
+Winkel 
+Schnitt / windschief / parallel 
+Ebenenformen 
+Abstand, Orthogonalität 
+Magenta Boxshadow (dark mode?) 
+Formelanzeige mit Berechnung */ 
